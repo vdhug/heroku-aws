ac15f00e0641cf0dcf0781d99bede49e47a451b8
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2020-01-25 13:52:25 -0500

    Allowing S3 access to allowed hosts

--- airline/settings.py
+++ airline/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '7!@8iekdn*_6e&x8m!mofryeaw775c&%-nc_)z)+p9w5@bw+(n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
